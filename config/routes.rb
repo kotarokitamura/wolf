7fe01fa5343ede85fe3ce5edde3ae8aaa74aaf00
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-26 11:39:30 +0900

    Created post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wolf::Application.routes.draw do
+  get "posts/index"
+  get "posts/show"
   #for omniauth
   get "/logout" => "sessions#destroy", as: "logout"
   get "/auth/:provider/callback", to: "sessions#create", as: "sessions"
