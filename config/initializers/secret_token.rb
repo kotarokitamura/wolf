0fffb482be8cb30d6bbcb781e3e43edd647f49c9
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-26 09:47:40 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wolf::Application.config.secret_key_base = 'ca70ed4661e1f2bb5f086f160a0001c67c140a7574a9eeeb075647964d79849af0dec5fa7255e8dc4e85dfc60ff9f03fa86fea2bb34d6a68d57f85a1ace968d3'
