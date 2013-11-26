8551f44a00a944a5929427e01fbc48d79b4f02d6
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-26 10:17:39 +0900

    Added omniauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter,"qQb1VxwVDyVJ4mRvmaZ0g","oO6V2rIlJOAJ1LSf5qgzd0KSkJCVvl3SfjSGEmr98"
+  provider :facebook,"381647438647066","d57272d602117cd8a50eeeebc5594525"
+end
