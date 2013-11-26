13aa89741f44e941bbf132801402e62bd0aaf6c0
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-26 10:49:39 +0900

    Added create auth account method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  validates :uid, presence: :true
+  validates :name, presence: :true
+
+  validates_uniqueness_of :uid
+
+  def self.create_with_omniauth(auth)
+    return false if auth["provider"] != "facebook"
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+    end
+  end
 end
