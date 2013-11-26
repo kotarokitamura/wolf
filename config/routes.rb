75d53b66ad3c26b9d1dc995893dc3bd30b4b8556
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-26 11:36:40 +0900

    Chaged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Wolf::Application.routes.draw do
   #for omniauth
-  resources :sessions, only: :destroy, as: "logout"
+  get "/logout" => "sessions#destroy", as: "logout"
   get "/auth/:provider/callback", to: "sessions#create", as: "sessions"
   root :to => "sessions#sign_in"
 end
