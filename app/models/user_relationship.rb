814bee0341c1905d0e012c369ca8dc24cccd6787
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2014-01-10 14:04:47 +0900

    #2: Compleated to create follow and unfollow function

--- app/models/user_relationship.rb
+++ app/models/user_relationship.rb
@@ -1,2 +1,5 @@
 class UserRelationship < ActiveRecord::Base
+  def self.get_relationship(current_user,params)
+    UserRelationship.where(["follower_id = ? and followed_id = ?", current_user.id, params[:id]]).first
+  end
 end
