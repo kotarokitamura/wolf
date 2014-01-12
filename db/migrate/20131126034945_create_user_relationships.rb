8e778b840ba89871d15f58e7ec95170cc74865dd
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2014-01-13 08:42:52 +0900

    #14: moved colomun last_checked_at

--- db/migrate/20131126034945_create_user_relationships.rb
+++ db/migrate/20131126034945_create_user_relationships.rb
@@ -3,6 +3,7 @@ class CreateUserRelationships < ActiveRecord::Migration
     create_table :user_relationships do |t|
       t.integer :follower_id
       t.integer :followed_id
+      t.timestamp :last_checked_at
 
       t.timestamps
     end
