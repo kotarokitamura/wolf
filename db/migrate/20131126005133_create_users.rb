05970520a1c069398256753189b9986101ba0f1d
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2014-01-13 08:06:41 +0900

    #9: Add colomun last checked_at in user

--- db/migrate/20131126005133_create_users.rb
+++ db/migrate/20131126005133_create_users.rb
@@ -8,6 +8,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :image_url
       t.string :email
       t.string :access_token
+      t.timestamp :last_checked_at
 
       t.timestamps
     end
