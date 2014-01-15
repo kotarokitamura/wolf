36d1f3ac5d5229a6aa1ef224192433de6d0fbffc
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2014-01-15 10:48:51 +0900

    #8: you can post the comment each post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wolf::Application.routes.draw do
-  resources :posts, :only => [:show, :new, :create, :update]
+  resources :posts, :only => [:show, :new, :create, :update], :shallow => true do
+    resources :comments, :only => [:index, :create, :new]
+  end
   resources :user_relationships, :only => [:update]
   resources :users, :only => [:index, :update, :show,:other_accounts, :twitter], :shallow => true do
     member do
