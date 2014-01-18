dd80379b561fb177e2ffa8b05f05b862875b015b
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2014-01-18 11:07:59 +0900

    #9: Added function content already checked or not

--- app/models/user_relationship.rb
+++ app/models/user_relationship.rb
@@ -1,10 +1,14 @@
 class UserRelationship < ActiveRecord::Base
+  belongs_to :user
 
   validates :user_id, presence: :true
   validates :followed_id, presence: :true
 
-  belongs_to :user
   def update_last_checked_time
     self.update_attributes(last_checked_at: Time.now)
+  end
+
+  def already_check?(user)
+    user.posts.first.posted_at > self.last_checked_at
   end
 end
