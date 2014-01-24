90ad0f5e9983c51813f2b5f73b40addf60a3065a
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2014-01-24 12:19:33 +0900

    #24: Added other_account spec

--- spec/models/other_account_spec.rb
+++ spec/models/other_account_spec.rb
@@ -5,11 +5,37 @@ describe OtherAccount do
   fixtures :users,:other_accounts
   before do
     @other_account = OtherAccount.new
-    @other_account
+    @other_account.user_id = 1
+    @other_account.provider = 'twitter'
+    @other_account.uid = '123456'
+    @other_account.name = 'Taro Yamada'
+    @other_account.email = 'taro.yamada@example.com'
   end
 
   context 'Added OtherAccount with some pattern' do
-    it 'should insert correct pattern' do
+    it 'should return true when insert correct pattern' do
+      @other_account.save
+      OtherAccount.last.uid.should == @other_account.uid
+    end
+
+    it 'should return false when insert name over max_length' do
+      @other_account.name = 'a' * ResourceProperty.name_max_length + 'a'
+      @other_account.save.should  be_false
+    end
+
+    it 'should return false when insert email over max_length' do
+      @other_account.name = 'a' * ResourceProperty.email_max_length + 'a'
+      @other_account.save.should  be_false
+    end
+
+    it 'should return false when insert uid nil' do
+      @other_account.uid = nil
+      @other_account.save.should  be_false
+    end
+
+    it 'should return false when insert user_id nil' do
+      @other_account.user_id = nil
+      @other_account.save.should  be_false
     end
   end
 end
